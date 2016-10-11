d78a74272f679064d19493e3454b755761b54337
luifeifei <1805008320@qq.com>
2016-10-11 12:27:03 +0800

    fuck again

---
+++ Gess.java
@@ -0,0 +1,24 @@
+public class Gess
+{
+	public static void main (String [] args){
+		char word = 'd' ,word1 = '@';
+		int p = 23045,p1 = 4513;
+	
+		System.out.println("你好\t百度");
+		System.out.println("在unicode标的\n位置是"+(int) word);
+		System.out.println("\"d在unicode标的位置:\"" + (int) word1);
+		System.out.println("d在unicode标的位置:" + (char) p);
+		System.out.println("d在unicode的位置"+ (char) p1);
+		Opint a = new Opint();
+		a.one();
+	}
+}
+
+class Opint
+{ 
+	public void one() {
+		System.out.println("liufei");
+	}
+		
+	
+}
